ffd7032f3fa20ce2e18628519c170b83a88b91ed
Josh Harvey <josh@lamassu.is>
2016-08-12 21:34:02 +0300

    WIP

--- http-server.js
+++ http-server.js
@@ -3,14 +3,47 @@ const app = express()
 require('express-ws')(app)
 const accounts = require('./accounts')
 
-app.get('/accounts/account/:account', function (req, res) {
-  accounts.getAccount(req.params.account)
-  .then(r => res.send(r))
-  .catch(logError)
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.get('/totem', (req, res) => {
+  return res.send('<the totem>')
+})
+
+app.get('/initial', (req, res) => {
+  // TODO: pull from plugins
+  const config = {
+    global: {
+      global: {
+        plugins: [{
+          code: 'twilio',
+          display: 'Twilio',
+          fields: [{
+            code: 'accountSid',
+            display: 'Account SID',
+            type: 'string',
+            secret: false,
+            required: true
+          }]
+        }]
+      }
+    }
+  }
+
+  return res.json({config: config})
+})
+
+app.get('/accounts/account/:account', function (req, res) {
+  accounts.getAccount(req.params.account)
+    .then(r => res.send(r))
+    .catch(logError)
+})
+
+app.listen(8093, function () {
+  console.log('lamassu-admin-server listening on port 8093')
 })
 
 app.ws('/echo', function (ws, req) {
