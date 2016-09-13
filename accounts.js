9a8ba50365178f5aed4131e9ca70299fc54f51c3
Josh Harvey <josh@lamassu.is>
2016-09-14 00:44:16 +0300

    account saving works

--- accounts.js
+++ accounts.js
@@ -4,13 +4,13 @@ const pify = require('pify')
 const fs = pify(require('fs'))
 const path = require('path')
 
-function fetchAccountData () {
+function fetchAccounts () {
   return db.oneOrNone('select data from user_config where type=$1', ['accounts'])
   .then(row => {
     return row
-    ? Promise.resolve(row)
-    : db.none('insert into user_config (type, data) values ($1, $2)', ['accounts', {}])
-      .then(fetchAccountData)
+    ? Promise.resolve(row.data.accounts)
+    : db.none('insert into user_config (type, data) values ($1, $2)', ['accounts', {accounts: []}])
+      .then(fetchAccounts)
   })
 }
 
@@ -23,55 +23,76 @@ function fetchAccountSchema (account) {
   .then(JSON.parse)
 }
 
-function mergeAccount (schema, newAccount) {
-  const dataFields = newAccount.fields
+function mergeAccount (oldAccount, newAccount) {
+  if (!newAccount) return oldAccount
 
-  const updateWithData = schemaField => {
-    const valueField = R.find(r => r.code === schemaField.code, dataFields)
-    const value = valueField ? valueField.value : null
-    return R.assoc('value', value, schemaField)
+  const newFields = newAccount.fields
+
+  const updateWithData = oldField => {
+    const newField = R.find(r => r.code === oldField.code, newFields)
+    const newValue = newField ? newField.value : null
+    return R.assoc('value', newValue, oldField)
   }
 
-  const updatedFields = schema.fields.map(updateWithData)
+  const updatedFields = oldAccount.fields.map(updateWithData)
 
-  return R.assoc('fields', updatedFields, schema)
+  return R.assoc('fields', updatedFields, oldAccount)
 }
 
-function getAccountRec (account) {
-  return fetchAccountSchema(account)
-  .then(schema => {
-    if (!schema) throw new Error('No such schema')
+function getAccounts (accountCode) {
+  return Promise.all([fetchAccountSchema(accountCode), fetchAccounts()])
+  .then(([schema, accounts]) => {
+    if (R.isEmpty(accounts)) return [schema]
+    const account = R.find(r => r.code === accountCode, accounts)
+    const mergedAccount = mergeAccount(schema, account)
 
-    return fetchAccountData()
-    .then(r => {
-      if (!r || R.isEmpty(r.data)) return schema
-
-      return mergeAccounts(schema, r.data.fields)
-    })
+    return updateAccounts(mergedAccount, accounts)
   })
 }
 
-function getAccount (account) {
-  return getAccountRec(account)
-  .then(dataFields => R.find(r => r.code === account, dataFields))
+function getAccount (accountCode) {
+  return getAccounts(accountCode)
+  .then(accounts => R.find(r => r.code === accountCode, accounts))
 }
 
-function save (accountRec) {
-  return db.none('update user_config set data=$1 where type=$2', [accountRec, 'accounts'])
+function save (accounts) {
+  return db.none('update user_config set data=$1 where type=$2', [{accounts: accounts}, 'accounts'])
 }
 
-function updateAccount (accountData) {
-  return getAccount(accountData.code)
-  .then(r => mergeAccount(r, accountData.fields))
-  .then(save)
-  .then(() => getAccount(accountData.code))
+function updateAccounts (newAccount, accounts) {
+  const accountCode = newAccount.code
+  const isPresent = R.any(R.propEq('code', accountCode), accounts)
+  const updateAccount = r => r.code === accountCode
+  ? newAccount
+  : r
+
+  return isPresent
+  ? R.map(updateAccount, accounts)
+  : R.append(accounts, [newAccount])
 }
 
+function updateAccount (account) {
+  return getAccounts(account.code)
+  .then(accounts => {
+    const merged = mergeAccount(R.find(R.propEq('code', account.code), accounts), account)
+    return save(updateAccounts(merged, accounts))
+  })
+  .then(() => getAccount(account.code))
+}
+
+// const newAccount = { code: 'twilio',
+//   display: 'Twilio',
+//   fields:
+//    [ { code: 'accountSid', value: 'xxx' },
+//      { code: 'authToken', value: '' },
+//      { code: 'fromNumber', value: '' },
+//      { code: 'toNumber', value: '' } ] }
+//
 // function pp (o) {
 //   console.log(require('util').inspect(o, {depth: null, colors: true}))
 // }
 //
-// getAccount('twilio')
+// updateAccount(newAccount)
 // .then(pp)
 // .then(() => process.exit(0))
 // .catch(e => { console.log(e.stack); process.exit(1) })
